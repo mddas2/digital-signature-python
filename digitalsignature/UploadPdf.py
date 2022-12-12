bac269dd1b92ca7b08e96b9646fecdd703139b13
mddas <mddas>
2022-12-12 02:30:22 -0800

    sign to pdf starting

--- digitalsignature/UploadPdf.py
+++ digitalsignature/UploadPdf.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from digitalsignature.models import Files
 
 # Create your views here.
 def PdfList(request):
+    files = Files.objects.all()
+
     data = {
-        'name' : 'manoj',
-        'class' : 'six',
+        'files' : files,
     }
     return render(request,'page_type/partials/list.html',data) #return render(request,'report/application-lists.html',data)
 
 def UploadPdf(request):
     if request.POST:
-        
-        return HttpResponse("this is upload pdf")
+        if 'unsigned_file' in request.FILES:
+            unsigned_file = request.FILES['unsigned_file']
+        else:
+            messages.error(request,'file can not be null')
+            return redirect(PdfList)
+
+    fileobj = Files()
+    fileobj.unsigned_file = unsigned_file
+    fileobj.user_id = 1
+    fileobj.save()
+    return redirect(PdfList)
+
--- digitalsignature/models.py
+++ digitalsignature/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 
 class Files(models.Model):
-    user = models.ForeignKey(, related_name="childs",on_delete=models.CASCADE,null=True)
+    user_id = models.IntegerField()
+    unsigned_file =  models.FileField(upload_to='user/documents', null=True)
+    signed_file =  models.FileField(upload_to='user/documents', null=True)
 
 # Create your models here.
