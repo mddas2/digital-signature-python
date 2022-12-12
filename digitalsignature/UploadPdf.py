5170fde159ac34c3a3ac30bbd5360a41fed65cda
mddas <mddas>
2022-12-12 00:26:22 -0800

    list.html

--- digitalsignature/UploadPdf.py
+++ digitalsignature/UploadPdf.py
@@ -8,4 +8,4 @@ def UploadPdf(request):
         'name' : 'manoj',
         'class' : 'six',
     }
-    return render(request,'page_type/list.html',data) #return render(request,'report/application-lists.html',data)
+    return render(request,'page_type/partials/list.html',data) #return render(request,'report/application-lists.html',data)
